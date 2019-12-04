1bf0789a1e1ca6dee4b3af4651dc0d999afcd58c
pdcxs <pdcxs@163.com>
2019-12-04 19:57:00 +0800

    updating prop2.py

--- Parabola/prop2.py
+++ Parabola/prop2.py
@@ -13,3 +13,59 @@ class OpenScene(Scene):
         self.wait(2)
         self.play(FadeOut(subs))
     
+class Prop2(Parabola):
+    CONFIG = {
+        'focus': 3,
+        'x_min': -5
+    }
+    def construct(self):
+        self.adjust_x_range()
+        graph = self.get_graph(color=DARK_BROWN)
+        directrix = self.get_directrix()
+        focus = Dot().move_to(self.get_focus())
+        focus.set_fill(DARK_BROWN)
+        focus.plot_depth = 1
+
+        self.play(ShowCreation(graph),
+            ShowCreation(directrix),
+            ShowCreation(focus))
+
+        h_line = Line()
+        d_loc = directrix.get_center()[0] * RIGHT
+        h_line.put_start_and_end_on(d_loc, ORIGIN)
+        h_line.plot_depth = -1
+        self.play(ShowCreation(h_line))
+
+        a = Dot()
+        a.set_fill(LIGHT_BROWN)
+        a.plot_depth = 1
+        a.move_to(self.coords_to_point(0, 0))
+
+        t = Dot()
+        t.set_fill(LIGHT_BROWN)
+        t.plot_depth = 1
+        t.move_to(d_loc)
+        
+        y_value = ValueTracker(9)
+        p = Dot()
+        p.set_fill(DARK_BLUE)
+        p.plot_depth = 1
+
+        p.add_updater(lambda m:\
+            m.move_to(self.value_to_point(
+                y_value.get_value())))
+        
+        m = Dot()
+        m.set_fill(DARK_BLUE)
+        m.plot_depth = 1
+        m.add_updater(lambda t:\
+            t.move_to(d_loc + p.get_center()[1] * UP))
+        
+        n = Dot()
+        n.set_fill(DARK_BLUE)
+        n.plot_depth = 1
+        n.add_updater(lambda m:\
+            m.move_to(p.get_center()[0] * RIGHT))
+        
+        self.play(*[ShowCreation(e) for e in\
+            [a, p, m, n, t]])
