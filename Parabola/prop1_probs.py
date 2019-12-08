0a5e2d875d644341e5953cebd7bd2f692ccb2177
pdcxs <pdcxs@163.com>
2019-12-08 12:04:54 +0800

    finish prop2 prob3

--- Parabola/prop1_probs.py
+++ Parabola/prop1_probs.py
@@ -319,3 +319,182 @@ class Prob2(Parabola):
         self.wait(3)
         self.play(ApplyMethod(q1_y.set_value, 3),
             ApplyMethod(p1_y.set_value, -9))
+        self.wait(10)
+
+
+class Prob3(Parabola):
+    CONFIG = {
+        'focus': 2,
+        'x_min': -4
+    }
+    def construct(self):
+        self.adjust_x_range()
+        graph = self.get_graph(color=LIGHT_BROWN)
+        directrix = self.get_directrix()
+        focus = Dot().move_to(self.get_focus())
+        focus.set_fill(DARK_BROWN)
+        focus.plot_depth = 1
+        focusLabel = TexMobject('F').scale(0.7)
+        focusLabel.next_to(focus, RIGHT)
+
+        self.play(*[ShowCreation(e) for\
+            e in [graph, directrix, focus, focusLabel]])
+
+        q1_y = ValueTracker(9)
+        q1 = Dot()
+        q1.set_fill(DARK_BLUE)
+        q1.plot_depth = 1
+
+        q1.add_updater(lambda m:\
+            m.move_to(self.coords_to_point(
+                self.func(q1_y.get_value()),
+                q1_y.get_value()
+            )))
+
+        q1_label = TexMobject('Q_1').scale(0.5)
+        q1_label.add_updater(lambda m:\
+            m.next_to(q1, RIGHT, buff=SMALL_BUFF))
+        
+        self.play(ShowCreation(q1), ShowCreation(q1_label))
+
+        q2 = Dot()
+        q2.set_fill(DARK_BLUE)
+        q2.plot_depth = 1
+
+        q2.add_updater(lambda m:\
+            m.move_to(self.get_opposite(q1)))
+
+        q2_label = TexMobject('Q_2').scale(0.5)
+        q2_label.add_updater(lambda m:\
+            m.next_to(q2, RIGHT, buff=SMALL_BUFF))
+
+        q1q2 = Line()
+        q1q2.add_updater(lambda m:\
+            m.put_start_and_end_on(
+                q1.get_center(),
+                self.get_opposite(q1)
+            ))
+
+        self.play(*[ShowCreation(e) for e in\
+            [q2, q2_label, q1q2]])
+
+        p1_y = ValueTracker(2)
+        p1 = Dot()
+        p1.set_fill(DARK_BLUE)
+        p1.plot_depth = 1
+
+        p1.add_updater(lambda m:\
+            m.move_to(self.coords_to_point(
+                self.func(p1_y.get_value()),
+                p1_y.get_value()
+            )))
+
+        p1_label = TexMobject('P_1').scale(0.5)
+        p1_label.add_updater(lambda m:\
+            m.next_to(p1, RIGHT, buff=SMALL_BUFF))
+        
+        self.play(ShowCreation(p1), ShowCreation(p1_label))
+
+        p2 = Dot()
+        p2.set_fill(DARK_BLUE)
+        p2.plot_depth = 1
+
+        p2.add_updater(lambda m:\
+            m.move_to(self.get_opposite(p1)))
+
+        p2_label = TexMobject('P_2').scale(0.5)
+        p2_label.add_updater(lambda m:\
+            m.next_to(p2, RIGHT, buff=SMALL_BUFF))
+
+        p1p2 = Line()
+        p1p2.add_updater(lambda m:\
+            m.put_start_and_end_on(
+                p1.get_center(),
+                self.get_opposite(p1)
+            ))
+
+        self.play(*[ShowCreation(e) for e in\
+            [p2, p2_label, p1p2]])
+
+        k1 = Dot()
+        k1.set_fill(DARK_BROWN)
+        k1.plot_depth = 1
+        k1.add_updater(lambda m:\
+            m.move_to(self.chord_to_directrix(p1, q1)))
+
+        k1_label = TexMobject('K_1').scale(0.5)
+        k1_label.add_updater(lambda m:\
+            m.next_to(k1, LEFT, buff=SMALL_BUFF))
+
+        p1q1 = Line()
+        p1q1.add_updater(lambda m:\
+            m.put_start_and_end_on(
+                self.right(p1, q1),
+                self.chord_to_directrix(p1, q1)
+            ))
+        p2q2 = Line()
+        p2q2.add_updater(lambda m:\
+            m.put_start_and_end_on(
+                self.right(p2, q2),
+                self.chord_to_directrix(p2, q2)
+            ))
+
+        self.play(*[ShowCreation(e) for e in \
+            [k1, k1_label, p1q1, p2q2]])
+
+        k2 = Dot()
+        k2.set_fill(DARK_BROWN)
+        k2.plot_depth = 1
+        k2.add_updater(lambda m:\
+            m.move_to(self.chord_to_directrix(p2, q1)))
+
+        k2_label = TexMobject('K_2').scale(0.5)
+        k2_label.add_updater(lambda m:\
+            m.next_to(k2, LEFT, buff=SMALL_BUFF))
+
+        p2q1 = Line()
+        p2q1.add_updater(lambda m:\
+            m.put_start_and_end_on(
+                self.right(p2, q1),
+                self.chord_to_directrix(p2, q1)
+            ))
+        p1q2 = Line()
+        p1q2.add_updater(lambda m:\
+            m.put_start_and_end_on(
+                self.right(p1, q2),
+                self.chord_to_directrix(p1, q2)
+            ))
+
+        self.play(*[ShowCreation(e) for e in \
+            [k2, k2_label, p2q1, p1q2]])
+
+        k1f = Line()
+        k1f.add_updater(lambda m:\
+            m.put_start_and_end_on(
+                k1.get_center(),
+                focus.get_center()
+            ))
+
+        k2f = Line()
+        k2f.add_updater(lambda m:\
+            m.put_start_and_end_on(
+                k2.get_center(),
+                focus.get_center()
+            ))
+
+        explain = TexMobject('K_1F \\perp K_2F')
+        explain.to_edge(RIGHT)
+
+        self.wait(2)
+        self.play(ShowCreation(k1f), ShowCreation(k2f))
+        self.wait(3)
+        self.play(Write(explain))
+        self.wait(5)
+
+        self.play(ApplyMethod(q1_y.set_value, 0.5),
+            ApplyMethod(p1_y.set_value, -3))
+
+        self.wait(3)
+        self.play(ApplyMethod(q1_y.set_value, 3),
+            ApplyMethod(p1_y.set_value, -9))
+        self.wait(10)
