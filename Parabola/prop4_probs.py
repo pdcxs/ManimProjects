3c67bd3f000bb2d5fb6e92aff435542964e8c4d1
Eric Brown <pdcxs@outlook.com>
2020-02-27 12:15:07 +0800

    Finish Prop4

--- Parabola/prop4_probs.py
+++ Parabola/prop4_probs.py
@@ -2,6 +2,7 @@ from manimlib.imports import *
 from ManimProjects.utils.geometry import *
 from ManimProjects.utils.Parabola import *
 from ManimProjects.utils.rate_functions import *
+from ManimProjects.utils.calculation import *
 
 TEX_SIZE = 0.65
 TEXT_SIZE = 0.3
@@ -287,8 +288,7 @@ class Prob2(Parabola):
         f.set_color(DARK_BROWN)
         f_label = Label(
             TexMobject('F').scale(TEX_SIZE),
-            f)
-        f_label.next_to(f, DOWN, buff=SMALL_BUFF)
+            f, direction=DOWN)
         f_label.plot_depth = 1
         directrix = self.get_directrix()
 
@@ -536,5 +536,336 @@ class Prob2(Parabola):
         self.wait(5)
 
         self.play(y_val.set_value, 6)
+        self.wait(15)
+
+class Prob3Open(Scene):
+    def construct(self):
+        line1_1 = CText('抛物线')
+        line1_1.set_fill(DARK_BLUE)
+
+        line1_2 = CText('性质四')
+        line1 = VGroup(line1_1, line1_2)
+        line1.arrange(RIGHT)
+        line2 = CText('推论三')
+        lines = VGroup(line1, line2)
+        lines.arrange(DOWN, buff=LARGE_BUFF)
+        self.play(Write(line1))
+        self.wait(3)
+        self.play(Write(line2))
+        self.wait(3)
+        
+        self.play(FadeOut(lines))
+
+class Prob3(Parabola):
+    CONFIG = {
+        "x_min": -8,
+        "focus": 2
+    }
+    def construct(self):
+        self.adjust_x_range()
+        graph = self.get_graph(color=DARK_BROWN)
+        f = Dot()
+        f.move_to(self.get_focus())
+        f.plot_depth = 1
+        f.set_color(DARK_BROWN)
+        f_label = Label(
+            TexMobject('F').scale(TEX_SIZE),
+            f, direction=RIGHT)
+        f_label.plot_depth = 1
+        directrix = self.get_directrix()
+
+        self.play(
+            *[ShowCreation(e) for e in\
+                [f, f_label, graph, directrix]]
+        )
+        self.wait()
+
+        y_val = ValueTracker(8)
+
+        p = Dot()
+        p.set_color(DARK_BLUE)
+        p.plot_depth = 1
+        p.add_updater(lambda m:\
+            m.move_to(self.value_to_point(
+                y_val.get_value())))
+        
+        p_label = Label(TexMobject('P').scale(TEX_SIZE), p, direction=UL)
+        p_label.plot_depth = 1
+
+        self.play(ShowCreation(p), ShowCreation(p_label))
+
+        p_label.make_dynamic()
+
+        q = Dot()
+        q.set_color(DARK_BLUE)
+        q.plot_depth = 1
+        q.add_updater(lambda m:\
+            m.move_to(self.get_opposite(p)))
+        
+        q_label = Label(TexMobject('Q').scale(TEX_SIZE), q, direction=RIGHT)
+        q_label.plot_depth = 1
+
+        pq = Line()
+        pq.add_updater(lambda l:\
+            l.put_start_and_end_on(
+                p.get_center(),
+                self.get_opposite(p)
+            ))
+        self.play(ShowCreation(pq))
+        self.play(ShowCreation(q), ShowCreation(q_label))
+        q_label.make_dynamic()
+        self.wait()
+
+        tangent_p = Line()
+        self.add_tangent_extent_updater(tangent_p, p)
+
+        tangent_q = Line()
+        self.add_tangent_extent_updater(tangent_q, q)
+
+        self.play(ShowCreation(tangent_p))
+        self.play(ShowCreation(tangent_q))
+
+        self.wait()
+
+        vertical_line = Line()
+        f_x = f.get_center()[0]
+        vertical_line.put_start_and_end_on(
+            f_x * RIGHT + FRAME_Y_RADIUS * UP,
+            f_x * RIGHT + FRAME_Y_RADIUS * DOWN,
+        )
+        self.play(ShowCreation(vertical_line))
+
+        p1 = Dot()
+        p1.plot_depth = 1
+        p1.set_color(DARK_BLUE)
+
+        p1.add_updater(lambda m:\
+            m.move_to(get_intersect(
+                tangent_p, vertical_line, f
+            )))
+        
+        self.play(ShowCreation(p1))
+
+        p1_label = Label(
+            TexMobject('P_1').scale(TEX_SIZE),
+            p1, direction=UL
+        )
+        p1_label.make_dynamic()
+        self.play(ShowCreation(p1_label))
+        self.wait()
+
+        q1 = Dot()
+        q1.plot_depth = 1
+        q1.set_color(DARK_BLUE)
+
+        q1.add_updater(lambda m:\
+            m.move_to(get_intersect(
+                tangent_q, vertical_line, f
+            )))
+        
+        self.play(ShowCreation(q1))
+
+        q1_label = Label(
+            TexMobject('Q_1').scale(TEX_SIZE),
+            q1, direction=RIGHT
+        )
+        q1_label.make_dynamic()
+        self.play(ShowCreation(q1_label))
+        self.wait()
+
+        proof = CText('证明思路：')
+        sub_1 = TexMobject('M_1NFP_1')
+        sub_2 = CText('与')
+        sub_3 = TexMobject('M_2NFQ_1')
+        sub_4 = CText('是菱形')
+
+        for e in [proof, sub_2, sub_4]:
+            e.scale(TEXT_SIZE)
+        for e in [sub_1, sub_3]:
+            e.scale(TEX_SIZE)
+        
+        idea = VGroup(sub_1, sub_2, sub_3, sub_4)
+        idea.arrange(RIGHT, buff=SMALL_BUFF)
+
+        prf = VGroup(proof, idea)
+        prf.arrange(DOWN)
+        idea.align_to(proof, LEFT)
+        prf.move_to(RIGHT * 4 + 1 * DOWN)
+
+        prob3 = TexMobject('P_1F=Q_1F')
+        prob3.scale(TEX_SIZE)
+        prob3.align_to(prf, LEFT)
+
+        self.play(ShowCreation(prob3))
+
+        p1f = Line()
+        p1f.set_color(RED)
+        p1f.add_updater(lambda m:\
+            m.put_start_and_end_on(
+                p1.get_center(),
+                f.get_center()
+            ))
+        self.play(ShowCreation(p1f))
+
+        q1f = Line()
+        q1f.set_color(RED)
+        q1f.add_updater(lambda m:\
+            m.put_start_and_end_on(
+                q1.get_center(),
+                f.get_center()
+            ))
+        self.play(ShowCreation(q1f))
+        self.wait()
+
+        self.play(
+            WiggleOutThenIn(p1f),
+            WiggleOutThenIn(q1f))
+        
+        self.play(y_val.set_value, 2)
+        self.wait(5)
+
+        self.play(y_val.set_value, 5)
+        self.wait(5)
+
+        self.play(y_val.set_value, 9)
+        self.wait(5)
+
+        n = Dot()
+        n.set_color(DARK_BLUE)
+        n.plot_depth = 1
+        n.add_updater(lambda m:\
+            m.move_to(get_intersect(
+                tangent_p,
+                tangent_q, f
+            )))
+        
+        self.play(ShowCreation(n))
+
+        n_label = Label(
+            TexMobject('N').scale(TEX_SIZE),
+            n, direction=LEFT
+        )
+        n_label.make_dynamic()
+        self.play(ShowCreation(n_label))
+        self.wait()
+
+        m1 = Dot()
+        m1.plot_depth = 1
+        m1.set_color(DARK_BLUE)
+        self.add_directrix_point_updater(p, m1)
+
+        self.play(ShowCreation(m1))
+
+        m1_label = Label(
+            TexMobject('M_1').scale(TEX_SIZE),
+            m1, direction=LEFT
+        )
+        m1_label.make_dynamic()
+        self.play(ShowCreation(m1_label))
+        self.wait()
+
+        pm1 = Line()
+        pm1.add_updater(lambda m:\
+            m.put_start_and_end_on(
+                p.get_center(),
+                m1.get_center()
+            ))
+        
+        self.play(ShowCreation(pm1))
+
+        m2 = Dot()
+        m2.plot_depth = 1
+        m2.set_color(DARK_BLUE)
+        self.add_directrix_point_updater(q, m2)
+
+        self.play(ShowCreation(m2))
+
+        m2_label = Label(
+            TexMobject('M_2').scale(TEX_SIZE),
+            m2, direction=LEFT
+        )
+        m2_label.make_dynamic()
+        self.play(ShowCreation(m2_label))
+        self.wait()
+
+        qm2 = Line()
+        qm2.add_updater(lambda m:\
+            m.put_start_and_end_on(
+                q.get_center(),
+                m2.get_center()
+            ))
+        
+        self.play(ShowCreation(qm2))
+        self.wait()
+
+        nf = Line()
+        nf.add_updater(lambda m:\
+            m.put_start_and_end_on(
+                n.get_center(),
+                f.get_center()
+            ))
+        self.play(ShowCreation(nf))
+        self.wait()
+
+        m1p1 = Line()
+        m1p1.add_updater(lambda m:\
+            m.put_start_and_end_on(
+                m1.get_center(),
+                p1.get_center()
+            ))
+        self.play(ShowCreation(m1p1))
+        self.wait()
+
+        m2q1 = Line()
+        m2q1.add_updater(lambda m:\
+            m.put_start_and_end_on(
+                m2.get_center(),
+                q1.get_center()
+            ))
+        self.play(ShowCreation(m2q1))
+        self.wait()
+
+        m1nfp1 = Polygon(
+            *[e.get_center() for e in\
+                [m1, n, f, p1]]
+        )
+        m1nfp1.add_updater(lambda m:\
+            m.set_points_as_corners(
+                [e.get_center() for e in\
+                    [m1, n, f, p1]]
+            ))
+        m1nfp1.stroke_width = 0
+        m1nfp1.set_fill(ORANGE, opacity=0.5)
+        self.play(ShowCreation(m1nfp1))
+        self.wait()
+
+        m2nfq1 = Polygon(
+            *[e.get_center() for e in\
+                [m2, n, f, q1]]
+        )
+        m2nfq1.add_updater(lambda m:\
+            m.set_points_as_corners(
+                [e.get_center() for e in\
+                    [m2, n, f, q1]]
+            ))
+        m2nfq1.stroke_width = 0
+        m2nfq1.set_fill(BLUE, opacity=0.5)
+        self.play(ShowCreation(m2nfq1))
+        self.wait()
+
+        self.play(ShowCreation(prf))
+
+        self.wait(10)
+
+        self.play(y_val.set_value, 6)
+        self.wait(5)
+
+        self.play(y_val.set_value, 2)
+        self.wait(5)
+
+        self.play(y_val.set_value, 4)
+        self.wait(5)
+
+        self.play(y_val.set_value, 9)
         self.wait(15)
 
