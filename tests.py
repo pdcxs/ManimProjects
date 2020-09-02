a40ca436bf89ed7fadac5533cf91224b7e7d5129
Eric Brown <pdcxs@outlook.com>
2020-09-02 14:43:16 +0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -385,3 +385,24 @@ class MultiSpring(Scene):
         self.wait(10)
         self.play(fix_point.shift, LEFT * 6, run_time=2)
         self.wait(15)
+
+# put zhang.mp4 into assets/videos folder
+class VideoTest(Scene):
+    def construct(self):
+        video = VideoMobject("zhang", scene=self)
+        video.start()
+
+        self.add(video)
+        self.wait(video.duration/2)
+        video.stop()
+        
+        text = Text("现在停顿1秒",
+            font='Microsoft YaHei')
+        text.scale(0.8)
+        text.move_to(UP*2)
+
+        self.play(Write(text))
+        self.wait()
+        
+        video.start()
+        self.wait(video.duration/2)
